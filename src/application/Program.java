33fa4f9637a5927612f17dbc0f1c8651e61fc485
Vitor Lima <vitor.xlr8@gmail.com>
2021-06-16 22:07:58 -0400

    Soma entre dois numeros

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,26 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 	public static void main(String[] args) {
 
-		System.out.println("Hello world");
+		System.out.println("Adição de dois números inteiros");
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		int soma;
+
+		System.out.print("Insira um número: ");
+		int n1 = sc.nextInt();
+		System.out.print("Insira outro número: ");
+		int n2 = sc.nextInt();
+		sc.close();
+
+		soma = n1 + n2;
+
+		System.out.println("A soma entre " + n1 + " e " + n2 + " é igual a: " + soma);
 	}
 
 }
